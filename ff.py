912113385c6b9c727c99f780c2c4ccb59b4fbd3b
q235-uu <55547151+q235-uu@users.noreply.github.com>
2019-09-19 19:58:35 +0300

    Create ff.py

---
+++ ff.py
@@ -0,0 +1,39 @@
+from graph import *
+windowSize(1200,800)
+canvasSize(1200,800)
+penSize(0)
+
+brushColor("#fed5a2")
+rectangle(0, 0, 1200, 200)
+brushColor("#fed5c4")
+rectangle(0, 200, 1200, 400)
+brushColor("#fed594")
+rectangle(0, 400, 1200, 600)
+brushColor("#b38694")
+rectangle(0, 600, 1200, 800)
+
+penColor("yellow")
+brushColor("yellow")
+circle(600,100,60)
+
+penColor("#fc9831")
+brushColor("#fc9831")
+polygon([(250,250),(500,300),(0,400)])
+polygon([(850,150),(1200,200),(700,300)])
+     
+penColor("red")
+brushColor("red")
+circle(600,500,50)
+polygon([(0,600),(300,400),(600,550)])
+polygon([(600,550),(850,400),(1200,500)])
+
+def qq():
+    moveTo(0,600)
+    
+
+
+
+polygon([(600,0),(596,2),(1200,500)]
+
+
+run()
