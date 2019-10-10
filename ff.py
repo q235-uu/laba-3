799740658cb6c0431a2d96f4d6e8c57c3f00e315
Маша <emelianenko.ma@phystech.edu>
2019-10-10 16:36:54 +0300

    'zzzzzzzzz'

--- ff.py
+++ ff.py
@@ -1,17 +1,17 @@
-from graph import *
+import graph
 
 sunset = None
 
 
 def sun():
     global sunset
-    brushColor('black')
-    sunset = circle(x0, y0, 30)
+    graph.brushColor('black')
+    sunset = graph.circle(x0, y0, 30)
 
 
 def sun_move():
     global x0, y0, sunset
-    moveObjectTo(sunset, x0, y0)
+    graph.moveObjectTo(sunset, x0, y0)
     x0 += 3
     y0 += 2
     if x0 > 1200:
@@ -21,50 +21,50 @@ def sun_move():
 
 
 def bird(x, y, color):
-    penColor(color)
-    line(x, y, x + 20, y)
-    line((x + 20), y, (x + 37), y + 10)
+    graph.penColor(color)
+    graph.line(x, y, x + 20, y)
+    graph.line((x + 20), y, (x + 37), y + 10)
 
 
 def cir(a, b, r):
-    brushColor('yellow')
-    circle(a, b, r)
+    graph.brushColor('yellow')
+    graph.circle(a, b, r)
 
 
-windowSize(1200, 800)
-canvasSize(1200, 800)
-penSize(0)
+graph.windowSize(1200, 800)
+graph.canvasSize(1200, 800)
+graph.penSize(0)
 
-brushColor("#fed5a2")
-rectangle(0, 0, 1200, 200)
-brushColor("#fed5c4")
-rectangle(0, 200, 1200, 400)
-brushColor("#fed594")
-rectangle(0, 400, 1200, 600)
-brushColor("#b38694")
-rectangle(0, 600, 1200, 800)
+graph.brushColor("#fed5a2")
+graph.rectangle(0, 0, 1200, 200)
+graph.brushColor("#fed5c4")
+graph.rectangle(0, 200, 1200, 400)
+graph.brushColor("#fed594")
+graph.rectangle(0, 400, 1200, 600)
+graph.brushColor("#b38694")
+graph.rectangle(0, 600, 1200, 800)
 
 
 def update():
     for i in range(1, 11):
         x = 1.1**i
         y = 3 * i
-        moveObjectBy(obj, x, y)
+        graph.moveObjectBy(obj, x, y)
 
 
-onTimer(update, 20)
+graph.onTimer(update, 20)
 
 
-penColor("#fc9831")
-brushColor("#fc9831")
-polygon([(250, 250), (500, 300), (0, 400)])
-polygon([(850, 150), (1200, 200), (700, 300)])
+graph.penColor("#fc9831")
+graph.brushColor("#fc9831")
+graph.polygon([(250, 250), (500, 300), (0, 400)])
+graph.polygon([(850, 150), (1200, 200), (700, 300)])
 
-penColor("red")
-brushColor("red")
-circle(600, 500, 50)
-polygon([(0, 600), (300, 400), (600, 550)])
-polygon([(600, 550), (850, 400), (1200, 500)])
+graph.penColor("red")
+graph.brushColor("red")
+graph.circle(600, 500, 50)
+graph.polygon([(0, 600), (300, 400), (600, 550)])
+graph.polygon([(600, 550), (850, 400), (1200, 500)])
 
 
 x = 20
@@ -91,9 +91,9 @@ for h in range(1, 100):
     b += 70
     r += 5
 sun()
-onTimer(sun_move, 100)
-penColor("yellow")
-brushColor("yellow")
-obj = circle(600, 100, 60)
+graph.onTimer(sun_move, 100)
+graph.penColor("yellow")
+graph.brushColor("yellow")
+obj = graph.circle(600, 100, 60)
 
-run()
+graph.run()
